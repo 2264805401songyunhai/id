dffc91b4386f86a5c70697c3d0397660ec6d1247
unknown <2264805401@qq.com>
2020-01-06 10:26:23 +0800

    set

--- app/src/router.js
+++ app/src/router.js
@@ -3,22 +3,22 @@ import loadable from '@/utils/loader'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import 'antd/dist/antd.less'
 
-const Login = loadable(() => import('./pages/login'))
-const Home = loadable(() => import('./pages/home'))
-const Register = loadable(() => import('./pages/register'))
+const Login = loadable(() => import('@/pages/Login'))
+const Home = loadable(() => import('@/pages/home'))
+const Register = loadable(() => import('@/pages/register'))
 
 export default class extends Component {
-    render() {
-        return (
-            <>
-                <BrowserRouter>
-                    <Switch>
-                        <Route path='/register' component={Register} />
-                        <Route path='/home' component={Home} />
-                        <Route path='/' component={Login} />
-                    </Switch>
-                </BrowserRouter>
-            </>
-        )
-    }
+	render() {
+		return (
+			<>
+				<BrowserRouter>
+					<Switch>
+						<Route path='/register' component={Register} />
+						<Route path='/home' component={Home} />
+						<Route path='/' component={Login} />
+					</Switch>
+				</BrowserRouter>
+			</>
+		)
+	}
 }
