64e37b3fcbf32248ead1e41c953dd429b1090df4
bubupu <3010674713@qq.com>
2020-01-06 19:49:56 +0800

    name

--- app/src/pages/tenant/index.jsx
+++ app/src/pages/tenant/index.jsx
@@ -21,9 +21,7 @@ export default class componentName extends Component {
 						</Breadcrumb>
 					</div>
 					<p className="time">
-						时间 : {
-							dateStr
-						}
+						时间 : {dateStr}
 					</p>
 				</div>
 			</div>
